ed12796e1e65aec39158bec3afdf9db000919c66
Guilherme Oliveira <35435775+GuilhermeOads@users.noreply.github.com>
2019-08-08 10:40:37 -0300

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://backend.cloudcamp.cf/api';
+const API_ROOT = 'https://backend.cloudcampgui.cf/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
